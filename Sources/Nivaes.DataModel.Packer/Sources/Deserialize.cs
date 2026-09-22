6afaf1aa48754e90de52341779727eb3b95f999e
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-09-22 17:19:13 +0200

    Optimization

--- Sources/Nivaes.DataModel.Packer/Sources/Deserialize.cs
+++ Sources/Nivaes.DataModel.Packer/Sources/Deserialize.cs
@@ -10,7 +10,7 @@ public static partial class DataModelPacker
     public static T? Deserialize<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(byte[] buffer /*ReadOnlySpan<byte> buffer*/)
           where T : IPackable<T>
     {
-        using var stream = new MemoryStream(buffer.ToArray(), writable: false);
+        using var stream = new MemoryStream(buffer, writable: false);
         using (var writer = new BinaryReader(stream, Encoding.UTF8))
         {
             return T.Deserialize(writer);
--- Sources/Nivaes.DataModel.Packer/Sources/Serialize.cs
+++ Sources/Nivaes.DataModel.Packer/Sources/Serialize.cs
@@ -9,13 +9,14 @@ public static partial class DataModelPacker
     public static byte[] Serialize<T>(in T value)
         where T : IPackable<T>
     {
-        var packable = (IPackable<T>)value;
-        using var stream = new MemoryStream();
+        using var stream = new MemoryStream(1024);
         using (var writer = new BinaryWriter(stream, Encoding.UTF8, leaveOpen: true))
         {
             value.Serialize(writer, value);
         }
 
-        return stream.ToArray();
+        return stream.TryGetBuffer(out var buffer)
+            ? buffer.AsSpan(0, checked((int)stream.Length)).ToArray()
+            : stream.ToArray();
     }
 }
