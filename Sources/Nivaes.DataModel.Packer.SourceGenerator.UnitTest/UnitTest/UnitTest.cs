2eafb18dd5888759c9be96f3751b7ba3d498d7b5
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-09-22 16:29:54 +0200

    Test

---
+++ Sources/Nivaes.DataModel.Packer.SourceGenerator.UnitTest/UnitTest/UnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using Nivaes.DataModel.Packer;
+
+namespace Nivaes.DataModel.Packer.SourceGenerator.UnitTest;
+
+public class UnitTest
+{
+    [Fact]
+    public void Test()
+    {
+    }
+}
